cc50c51c1a45fec2bfb2962500e1a9356738f85f
ElSheNawY <dev.mahmoud.elshenawy@gmail.com>
2022-07-16 21:50:22 +0200

    edit main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'buttons_widget.dart';
 
-
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
